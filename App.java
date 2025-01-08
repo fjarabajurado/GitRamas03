0261e9dfba16fd2407016f6e9d7f4b56e7b01c75
fjarabajurado <fj.jarabajurado@edu.gva.es>
2025-01-08 17:15:25 +0100

    Utilizao Persona

--- App.java
+++ App.java
@@ -1,5 +1,23 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Persona carmen = new Persona("Carmen Pérez", 34);
+        Persona luisa = new Persona ("Luisa Segarra", 52);
+        Familia fam = new Familia(carmen);
+        System.out.println("La edad de luisa es: " + luisa.getEdad());
+
+        System.out.println(carmen.toString());
+        System.out.println(luisa.toString());
+
+        System.out.println("Medida " + carmen.getNombre() + " = " + carmen.largoNombre());
+        System.out.println("Medida " + luisa.getNombre() + " = " + luisa.largoNombre());
+
+        // muestra la familia
+        System.out.println("\nFamilia");
+        System.out.println(fam.toString());
+        
+
+        fam.add(luisa);
+        fam.add(new Persona("Frank de la Jungla", 48));
+        System.out.println(fam.toString());
     }
 }
